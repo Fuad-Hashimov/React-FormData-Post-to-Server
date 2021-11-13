62c6906b38a68819d91ad43344d8f84202008cb1
FuadHashimov91 <fuadbh@code.edu.az>
2021-11-14 00:20:44 +0400

    Add Redirect Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from "react";
 import Home from "./Components/Home";
 import Nav from "./Components/Nav";
-import { Switch, Route } from "react-router";
+import { Switch, Route, Redirect } from "react-router";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import FormPage from "./Components/FormPage";
 
 export default function App() {
-
-
-  
   return (
     <div>
       <Nav />
       <Switch>
-        {/* <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} /> */}
         <Route exact path="/">
           <Home />
         </Route>
@@ -29,9 +23,9 @@ export default function App() {
         <Route path="/form">
           <FormPage />
         </Route>
-        {/* <Route>
+        <Route>
           <Redirect to="/" />
-        </Route> */}
+        </Route>
       </Switch>
     </div>
   );
